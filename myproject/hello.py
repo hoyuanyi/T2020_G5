30ffbac3dcfd4c1c161d4eaac29d8920cb73b689
huangz27 <57166880+huangz27@users.noreply.github.com>
2019-10-31 11:45:42 +0800

    customerID

--- myproject/hello.py
+++ myproject/hello.py
@@ -1,6 +1,19 @@
 from flask import Flask 
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
+@app.route('/getCustomerId/<name>')
+def index(name):
+    headers = {
+    'identity': 'Group24',
+    'token': 'b8f08d3a-47b2-4238-86bf-a8ed776352c2'
+    }
+    r = requests.get(
+      'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/'+name,
+      headers = headers)
+    return r.json()
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
