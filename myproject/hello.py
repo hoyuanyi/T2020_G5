8c48ffe2df859fff806bd6a2b31b8ca0d050c9fe
huangz27 <57166880+huangz27@users.noreply.github.com>
2019-10-31 13:27:36 +0800

    updated backend API

--- myproject/hello.py
+++ myproject/hello.py
@@ -1,10 +1,11 @@
 from flask import Flask 
 import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/getCustomerId/<name>')
-def index(name):
+def CustomerId(name):
     headers = {
     'identity': 'Group24',
     'token': 'b8f08d3a-47b2-4238-86bf-a8ed776352c2'
@@ -12,8 +13,64 @@ def index(name):
     r = requests.get(
       'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/'+name,
       headers = headers)
+
+    print(r.json()['customerId'])
     return r.json()
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/getCustomerDetails/<customerId>')
+def CustomerDetails(customerId):
+    headers = {
+    'identity': 'Group24',
+    'token': 'b8f08d3a-47b2-4238-86bf-a8ed776352c2'
+    }
+    r = requests.get(
+      'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/'+ customerId + '/details',
+      headers = headers)
+    return r.json()
+
+@app.route('/getAccountlist/<customerId>')
+def Accountlist(customerId):
+    headers = {
+    'identity': 'Group24',
+    'token': 'b8f08d3a-47b2-4238-86bf-a8ed776352c2'
+    }
+    r = requests.get(
+      'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/'+ customerId,
+      headers = headers)
+    return json.dumps(r.json())
+
+@app.route('/getBalance/<accountId>')
+def DepositBalance(accountId):
+    headers = {
+    'identity': 'Group24',
+    'token': 'b8f08d3a-47b2-4238-86bf-a8ed776352c2'
+    }
+    r = requests.get(
+      'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/'+ accountId +'/balance?month=1&year=2018',
+      headers = headers)
+    print(r.json())
+    return r.json()
+
+@app.route('/getCreditAccount/<customerId>')
+def CreditAccountList(customerId):
+    headers = {
+    'identity': 'Group24',
+    'token': 'b8f08d3a-47b2-4238-86bf-a8ed776352c2'
+    }
+    r = requests.get(
+      'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/credit/'+ customerId,
+      headers = headers)
+  
+    return json.dumps(r.json())
+
+@app.route('/getOutstandingBalance/<accountId>')
+def OutstandingBalance(accountId):
+    headers = {
+    'identity': 'Group24',
+    'token': 'b8f08d3a-47b2-4238-86bf-a8ed776352c2'
+    }
+    r = requests.get(
+      'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/credit/' + accountId + '/balance',
+      headers = headers)
+    print(r.json())
+    return r.json()
